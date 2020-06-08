c7524029af83ee2d8efe1e0c607612245048ab56
dheerajvora <dheerajvora1998@gmail.com>
2020-06-08 02:22:28 -1200

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+    $(".colas").click(function(){
+        $(".navlist").toggleClass("active")
+    })
+});
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    nav:false,
+    autoplay:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
